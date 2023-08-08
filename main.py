b62798521a99c24ae4f22ae073146e844824f326
kishanrajeev <kishanrajeev>
2023-08-08 18:43:11 -0400

    Added basic reply functionality

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
-import k2key
+#importing other python files
+import k2key #Contails API key
+
+#Importing libraries
 import google.generativeai as palm
 
 palm.configure(api_key=k2key.API_KEY)
 
 # Create a new conversation
-response = palm.chat(messages='Hello')
+response = input("Star a new conversation: ")
 
 # Last contains the model's response:
 response.last
 
+print(response.last)
+
+
+# Add to the existing conversation by sending a reply
+response = response.reply(input("Respond to Palm: "))
+# See the model's latest response in the `last` field:
+response.last
 print(response.last)
