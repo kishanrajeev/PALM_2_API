62ecbe4fcbd3feabda2babae9e523848d8a0d976
kishanrajeev <kishanrajeev>
2023-08-08 20:07:13 -0400

    Added basic continuity

--- main.py
+++ main.py
@@ -4,12 +4,23 @@ import k2key #Contails API key
 #Importing libraries
 import google.generativeai as palm
 
-palm.configure(api_key=k2key.API_KEY)
+def new_palm():
 
-response = palm.chat(messages=(input('Start a new conversation: ')), temperature = 1)
-response.last
-print(response.last)
+    Og_response = input("Start a new conversation: ")
+    if Og_response == 'n':
+       new_palm()
+    else:
+        palm.configure(api_key=k2key.API_KEY)
 
-response = response.reply(input("Respond to Palm: "))
-response.last
-print(response.last)
+        response = palm.chat(messages=(Og_response), temperature = 1)
+        response.last
+        print(response.last)
+
+        response = response.reply(input('Respond to Palm: '))
+        response.last
+        print(response.last)
+
+
+if __name__ == '__main__':
+    while True:
+        new_palm()
