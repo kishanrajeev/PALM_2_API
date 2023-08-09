a76de7f69e8dedd3921b4935e1771c6d4d472afe
kishanrajeev <kishanrajeev>
2023-08-08 20:34:50 -0400

    Added exit function

--- main.py
+++ main.py
@@ -16,6 +16,8 @@ def new_palm():
     if Og_response == 'n':
         clear_console()
         new_palm()
+    elif Og_response == 'exit':
+        quit()
     else:
         palm.configure(api_key=k2key.API_KEY)
 
@@ -26,6 +28,8 @@ def new_palm():
             if Og2_response == 'n':
                 clear_console()
                 new_palm()
+            elif Og2_response == 'exit':
+                quit()
             else:
                 response = response.reply(Og2_response)
                 print(response.last)
